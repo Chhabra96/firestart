57daf4cbb1dba02a5b911eeef47eb256bdd790d8
Priya Chhabra <66823080+Chhabra96@users.noreply.github.com>
2020-08-31 12:48:50 +0530

    Create 06.c

---
+++ C Language/06.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+
+{
+    const char greetings[] = "String Literal";
+    const int n = 99;
+
+    puts(greetings);
+    printf("N is %d\n",n);
+
+    return (0);
+
+}
