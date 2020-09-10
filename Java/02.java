622f23eb18c753d386f573f0923ee6e53efe1f08
Priya Chhabra <66823080+Chhabra96@users.noreply.github.com>
2020-09-10 19:27:33 +0530

    Create 02.java

---
+++ Java/02.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int studentage = 20;
+        double studentGPA = 4.5;
+        char studentFirstintial = 'P';
+        char studentLastintial = 'C';
+        boolean hasPerfectAttendance = true;
+        String studentName = "Priya Chhabra";
+
+        System.out.println(studentName);
+        System.out.println(studentage);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstintial);
+        System.out.println(studentLastintial);
+        System.out.println(hasPerfectAttendance);
+
+
+
+    }
+
+   /* Primitive Data type
+    Boolean represent true or false value
+    int represrnt a whole number
+    Double represent a decimal number
+    char represent a single letter or symbol
+         */
+
+}
