9142567a9515fab340f915d3d32216a0db1077c9
Priya Chhabra <66823080+Chhabra96@users.noreply.github.com>
2020-09-11 13:45:02 +0530

    Create 05.java

---
+++ Java/05.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+     /* Relational Operator
+       less than sign <
+       greater than sign  >
+       equals to  ==
+        less than or equal to  <=
+        greater than or equals to  >=
+        not equal to  !=
+      */
+
+        System.out.println("Pick a number between 1 and 10");
+        Scanner scanner = new Scanner(System.in);
+
+        int inputtedNum = scanner.nextInt();
+
+        if (inputtedNum < 5){
+            System.out.println("Enjoy the  good luck a friend brings you");
+        }
+        else {
+            System.out.println("Your shoe selection will make you happy today ");
+    }
+
+    }
+
+}
