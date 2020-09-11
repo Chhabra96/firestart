53c6bff99ff3cff4daf3e71ed9733ccb6006d2dd
Priya Chhabra <66823080+Chhabra96@users.noreply.github.com>
2020-09-11 13:15:19 +0530

    Create 03.java

---
+++ Java/03.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+     // Concatenating Strings in Java
+        double studentGPA =3.2;
+        String studentFirstName = "Priya";
+        String studentLastName = "Chhabra";
+
+        System.out.println(studentFirstName+" "+studentLastName + "  has a GPA of "+ studentGPA);
+
+    }
+
+}
