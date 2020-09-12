a8c5f366177d9815bb8e9f537178a67d4963a4ce
Priya Chhabra <66823080+Chhabra96@users.noreply.github.com>
2020-09-12 13:39:19 +0530

    Create 11.java

---
+++ Java/11.java
@@ -0,0 +1,24 @@
+public class Main{
+
+    //Dynamic Function
+
+    public static double calculateTotalMealPrice(double listedMealPrice,
+                                               double tipRate, double taxRate){
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice +tip+ tax;
+        return result;
+    }
+
+    public static void main(String[] args){
+
+        //Calling the function
+
+        double groupTotalMealPrice = calculateTotalMealPrice(100,.2,.08);
+        System.out.println(groupTotalMealPrice);
+
+        double individualMealPrice = groupTotalMealPrice/5;
+        System.out.println(individualMealPrice);
+
+    }
+}
