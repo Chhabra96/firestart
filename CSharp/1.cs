d0702d53a73826e29ecbdce8b436eb0dd0534129
Priya Chhabra <66823080+Chhabra96@users.noreply.github.com>
2020-12-06 16:42:39 +0530

    Create 1.cs

---
+++ CSharp/1.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("First Program in C#");
+            var a = "My String";
+            Console.WriteLine(a);
+
+            var b = Console.ReadLine();
+            Console.WriteLine(b);
+        }
+    }
+}
